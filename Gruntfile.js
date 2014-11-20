c92ca78332ac41f6fa930f8db1e53fc0c580d54a
Alexey Mazurik <alexeymazurik@gmail.com>
2014-11-21 01:23:05 +0200

    added connect lib

--- Gruntfile.js
+++ Gruntfile.js
@@ -51,6 +51,7 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-cssmin');
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-connect');    
 
     grunt.registerTask('default', ['sass', 'cssmin', 'clean', 'connect::keepalive']);
 
